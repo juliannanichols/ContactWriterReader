b2cc604fe2243882fba3bdb2fa17d59abd7d783b
juliannanichols <junichols17@saintjosephhs.com>
2015-12-10 09:59:47 -0500

    big

--- src/ContactWriterReader.java
+++ src/ContactWriterReader.java
@@ -13,26 +13,7 @@ public class ContactWriterReader {
 	 * @param args
 	 */
 	public static void main(String[] args) throws IOException {
-		File contacts = new File( "myContacts.txt" );
-		Scanner keyboard = new Scanner( System.in );
-		String newName;
 		
-		while( !contacts.exists() ) {
-        	System.out.print( contacts.getName() + " DOES NOT exist. New name: " );
-            newName = keyboard.nextLine();
-            contacts = new File( newName );
-        }
-		
-		while( contacts.exists() ) {
-			
-		}
-		
-		/*
-		 * first time through start loop...asks user for information..like loop already in contact class
-		 * another loop that goes through and writes each individual piece of information to disk
-		 * 
-		 * second time through..read back with header
-		 */
 	}
 
 }
