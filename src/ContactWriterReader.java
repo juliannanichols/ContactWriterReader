0ca1f9b1d64e879fe52b233c5a6de1342da3e23a
juliannanichols <junichols17@saintjosephhs.com>
2015-12-12 20:30:19 -0500

    closer..

--- src/ContactWriterReader.java
+++ src/ContactWriterReader.java
@@ -13,29 +13,15 @@ public class ContactWriterReader {
 	public static void main(String[] args) throws IOException {
 		
 		File contacts = new File( "myContacts.txt" );
-		Contacts [] myContacts;
-		myContacts = new Contacts[25];
-		int numContacts = 0;
-		Scanner keyboard = new Scanner( System.in );
-		String info;
+		PrintWriter write = new PrintWriter(contacts); //PrintWriter used to write to file
+		Scanner keyboard = new Scanner( System.in ); //Scanner object
+		Contacts [] myContacts = new Contacts[25];
+		BufferedReader reader = new BufferedReader( new FileReader(contacts) );;
 		String input;
-		PrintWriter write;
-		BufferedReader reader;
-		
-		/*
-		 * File myFile = new File( "my_contacts.txt" );
-		 * Contact [] myContacts;
-		 * myContacts = new Contact[25];
-		 * int numContacts = 0; tells where we are in the array
-		 * Scanner keyboard = new Scanner( System.in );
-		 * String input;
-		 * PrintWriter writer;
-		 * BufferedReader reader;
-		 */
+		int numContacts = 0;
 		
 		if( contacts.exists() ) {
 			
-			reader = new BufferedReader( new FileReader(contacts) );
 			while( reader.ready() ) {
 			myContacts [numContacts] = new Contacts();
 			input = reader.readLine();
@@ -64,66 +50,66 @@ public class ContactWriterReader {
 			
 			numContacts++;
 			
-			reader.close();
+			reader.close(); //reader needs to be closed
+			
 				for( int ctr = 0; ctr < numContacts; ctr++ ) {
-					//printing out still
+					int contNum = 1;
+					System.out.print( "Contact number " + contNum + ":\nName: "
+							+ myContacts[ctr].getPrefix() + " " + myContacts[ctr].getFirstName() + " "
+							+ myContacts[ctr].getMiddleName() + " " + myContacts[ctr].getLastName()
+							+ "\nPhone Number: " + myContacts[ctr].getPhoneNumber() + "\nE-mail: "
+							+ myContacts[ctr].getEmail() + "\nAddress: " + myContacts[ctr].getStreet()
+							+ "\n" + myContacts[ctr].getCity() + ", " + myContacts[ctr].getState() + " "
+							+ myContacts[ctr].getZip() + myContacts[ctr].getOccupation() );
 				}
 			}
 			
 		} else {
+			
+			//getting all the contact information
 			do {
 				myContacts[numContacts] = new Contacts();
 				
-				 //last name
+				//last name
 		        System.out.print( "Enter last name: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setLastName( input );
-		        
 		        //first name
 		        System.out.print( "Enter first name: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setFirstName( input );
-		        
 		        //middle name
 		        System.out.print( "Enter middle name: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setMiddleName( input );
-		        
 		        //prefix
 		        System.out.print( "Enter prefix: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setPrefix( input );
-		        
 		        //phone number
 		        System.out.print( "Enter phone number (### - ### - ####): " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setPhoneNumber( input );
-		        
 		        //e-mail address
 		        System.out.print( "Enter e-mail: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setEmail( input );
-		        
 		        //street name
 		        System.out.print( "Enter street name: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setStreet( input );
-		        
 		        //city name
 		        System.out.print( "Enter city name: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setCity( input );
-		        
 		        //state name
 		        System.out.print( "Enter state name: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setState( input );
-		        
 		        //zip code
 		        System.out.print( "Enter zip code: " );
 		        input = keyboard.nextLine();
 		        myContacts[numContacts].setZip( input );
-		        
 		        //occupation
 		        System.out.print( "Enter occupation: " );
 		        input = keyboard.nextLine();
@@ -131,46 +117,28 @@ public class ContactWriterReader {
 		        
 		        System.out.print( "Do you want to enter another contact? (Y/N): ");
 		        input = keyboard.nextLine();
+		        numContacts++;
+		        
 			} while (input.equalsIgnoreCase("y"));
 			
 		}
 		
-		/*
-		 * for( int ctr = 0; ctr < numContacts; ctr++ ) {
-		 * System.out.print( "Last name: " +myContacts[ctr].getLastName() );
-		 * System.out.print( "First name: " + myContacts[ctr].getFirstName() );
-		 * System.out.printf( "%s %s\n" , myContacts[ctr].getFirstName() , myContcacts[ctr].getLastName() );
-		 * System.out.print( "-----" ); 
-		 * }
-		 * 
-		 * } else {
-		 * if the file does not exist, create an array of ocntact objects and then write them to disk and exit the program
-		 * 
-		 * asking for input from keyboard for contact
-		 * do {
-		 * 	myContacts[numContacts] = new Contacts...create new contact object in array
-		 * 	System.out.print( "Last name: " );
-		 * 	input = keyboard.nextLine();
-		 * 	myContacts[numContacts].setFirstName(input);
-		 *  ....
-		 *  at the end..
-		 *  System.out.print( "Do you want to enter another (y/n)? " );
-		 *  input = keyboard.nextLine().toLowerCase();
-		 *  
-		 *  increment counter
-		 *  numCounter++;
-		 * } while( input.equals("y));
-		 * 
-		 * writer = new PrintWriter(myFile);
-		 * loop through array and print
-		 * for( int ctr = 0; ctr < numContacts; ctr++ ) {
-		 * 		writer.println( myContacts[ctr].getLastName() );
-		 * 		writer.println( myCOntacts[ctr].getFirstName() );
-		 * 
-		 * close the file
-		 * writer.close();
-		 */
+		//writing information to file
+		for (int ctr = 0; ctr < numContacts; ctr++ ) {
+			write.println( myContacts[ctr].getFirstName() );
+			write.println( myContacts[ctr].getMiddleName() );
+			write.println( myContacts[ctr].getLastName() );
+			write.println( myContacts[ctr].getPrefix() );
+			write.println( myContacts[ctr].getPhoneNumber() );
+			write.println( myContacts[ctr].getEmail() );
+			write.println( myContacts[ctr].getStreet() );
+			write.println( myContacts[ctr].getCity() );
+			write.println( myContacts[ctr].getState() );
+			write.println( myContacts[ctr].getZip() );
+			write.println( myContacts[ctr].getOccupation() );
+		}
 		
+		write.close(); //close PrintWriter	
 	}
 
 }
